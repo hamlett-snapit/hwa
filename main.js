87e59bd15e928c0eb8ab92e36100f62c094b6126
hamlett-snapit <hamlett.snapit@gmail.com>
2020-04-29 14:40:54 -0500

    first application

---
+++ main.js
@@ -0,0 +1,5 @@
+// Use this line below to insert text or other things into your HTML page. You can simply copy and paste the same line below to add more items into your page. Be sure to add addtional placeholders like " <p id='demo'> </p> " and dont use the same id. 
+document.getElementById("word").innerHTML = "Hello World !"
+
+// Shown below is a copy of the line above with a different id. Be sure to place a matching placeholder in your HTML page
+document.getElementById("word2").innerHTML = "Ramone H"
